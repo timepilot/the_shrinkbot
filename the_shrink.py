ff6fa9f4b9b349763e60b01f57360d91ff046573
Vivek Haldar <vh@vivekhaldar.com>
2011-01-16 12:10:16 -0800

    Minor cleanups.

--- the_shrink.py
+++ the_shrink.py
@@ -19,20 +19,10 @@ if __name__ == '__main__':
     oauth_filename = os.environ.get('HOME', '') + os.sep + '.twitter_oauth'
     oauth_token, oauth_token_secret = read_token_file(oauth_filename)
 
-    twitter = Twitter(
-        auth=OAuth(
-            oauth_token, oauth_token_secret, CONSUMER_KEY, CONSUMER_SECRET),
-        secure=True,
-        api_version='1',
-        domain='api.twitter.com')
-
-    twitter.domain="search.twitter.com"
+    # We use two twitter clients, one to search, another to update. Just
+    # easier that way...
+    twitter = Twitter(domain='search.twitter.com')
     twitter.uriparts=()
-    # We need to bypass the TwitterCall parameter encoding, so we
-    # don't encode the plus sign, so we have to encode it ourselves
-    # query_string = "+".join(
-    #     [quote(term.decode('UTF-8'))
-    #      for term in options['extra_args']])
 
     last_id_replied = ''
 
@@ -41,32 +31,29 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         last_id_replied = sys.argv[1]
 
+    doctor = eliza.eliza()
+
+    poster = Twitter(
+        auth=OAuth(
+            oauth_token, oauth_token_secret, CONSUMER_KEY, CONSUMER_SECRET),
+        secure=True,
+        api_version='1',
+        domain='api.twitter.com')
+
     while True:
         results = twitter.search(q="@the_shrinkbot", since_id=last_id_replied)['results']
-        #print results
-
+    
         if not results:
             print 'No results this time...'
 
-        # Switch back to regular api domain.
-        #twitter.domain="api.twitter.com"
-
-        doctor = eliza.eliza()
-    
-        poster = Twitter(
-            auth=OAuth(
-                oauth_token, oauth_token_secret, CONSUMER_KEY, CONSUMER_SECRET),
-            secure=True,
-            api_version='1',
-            domain='api.twitter.com')
-    
-    
         for result in results:
+            # Remove my name from the question.
             question = result['text'].replace('@the_shrinkbot', '')
             asker = result['from_user']
             id = str(result['id'])
             print " <<< " + asker + ": " + question
             doctor_response = doctor.respond(question.strip())
+
             # We append part of the ID to avoid duplicates.
             msg = '@%s %s (%s)' % (asker, doctor_response, id[-4:])
             print '====> Resp = %s' % msg
